5d37f9f43542b5f3c3c4d7d2f7e1e07fd826c617
Stefan VanBuren <me@svbn.me>
2020-12-30 12:29:52 -0500

    refactor: Use reagent.core/with-let over let
    This allows for some of the form-2 components to become "form-1".

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,4 +1,5 @@
 (ns app.main
+  {:clj-kondo/config '{:lint-as {reagent.core/with-let clojure.core/let}}}
   (:require [reagent.dom :as rdom]
             [reagent.core :as r]
             [clojure.string :refer [starts-with?]]
@@ -9,13 +10,12 @@
   a button B. Initially, the value in T is '0' and each click of B increases
   the value in T by one."
   []
-  (let [cnt (r/atom 0)]
-    (fn []
-      [:section
-        [:h2 "Counter"]
-        [:input {:type "text" :readOnly true :value @cnt}]
-        [:button {:on-click #(swap! cnt inc)}
-         "Count"]])))
+  (r/with-let [cnt (r/atom 0)]
+    [:section
+      [:h2 "Counter"]
+      [:input {:type "text" :readOnly true :value @cnt}]
+      [:button {:on-click #(swap! cnt inc)}
+       "Count"]]))
 
 (defn temperature-converter
   "The task is to build a frame containing two textfields Tc and Tf
@@ -27,35 +27,35 @@
   Celsius into a temperature F in Fahrenheit is _C = (F-32) * (5/9)_ and the
   dual direction is _F = C * (9/5) + 32_."
   []
-  (let [temp (r/atom {}) ; starts off empty
-        to-fahrenheit (fn [celsius]
-                        (+ 32 (* 9 (/ celsius 5))))
-        to-celsius (fn [fahrenheit]
-                     (* (/ 5 9) (- fahrenheit 32)))]
-    (fn []
-      [:section
-       [:h2 "Temperature Converter"]
+  (r/with-let [temp (r/atom {}) ; starts off empty
+               to-fahrenheit (fn [celsius]
+                               (+ 32 (* 9 (/ celsius 5))))
+               to-celsius (fn [fahrenheit]
+                            (* (/ 5 9) (- fahrenheit 32)))]
 
-       [:input {:type "number"
-                :value (:celsius @temp)
-                :id "celsius"
-                :on-change (fn [e]
-                             (let [celsius (-> e .-target .-value)]
-                               (reset! temp {:celsius celsius
-                                             :fahrenheit (to-fahrenheit celsius)})))}]
+    [:section
+     [:h2 "Temperature Converter"]
 
-       [:label {:for "celsius"} "Celsius"]
+     [:input {:type "number"
+              :value (:celsius @temp)
+              :id "celsius"
+              :on-change (fn [e]
+                           (let [celsius (-> e .-target .-value)]
+                             (reset! temp {:celsius celsius
+                                           :fahrenheit (to-fahrenheit celsius)})))}]
 
-       [:span " = "]
+     [:label {:for "celsius"} "Celsius"]
 
-       [:input {:type "number"
-                :value (:fahrenheit @temp)
-                :id "fahrenheit"
-                :on-change (fn [e]
-                             (let [fahrenheit (-> e .-target .-value)]
-                               (reset! temp {:celsius (to-celsius fahrenheit)
-                                             :fahrenheit fahrenheit})))}]
-       [:label {:for "fahrenheit"} "Fahrenheit"]])))
+     [:span " = "]
+
+     [:input {:type "number"
+              :value (:fahrenheit @temp)
+              :id "fahrenheit"
+              :on-change (fn [e]
+                           (let [fahrenheit (-> e .-target .-value)]
+                             (reset! temp {:celsius (to-celsius fahrenheit)
+                                           :fahrenheit fahrenheit})))}]
+     [:label {:for "fahrenheit"} "Fahrenheit"]]))
 
 (defn flight-booker
   "The task is to build a frame containing a combobox C with the two options
@@ -69,71 +69,70 @@
   on 04.04.2014.'). Initially, C has the value 'one-way flight' and T1 as well
   as T2 have the same (arbitrary) date (it is implied that T2 is disabled)."
   []
-  (let [state (r/atom {:selected "one-way"
-                       :start-date "2020-08-22"
-                       :start-date-valid? true
-                       :end-date "2020-08-22"
-                       :end-date-valid? true})]
+  (r/with-let [state (r/atom {:selected "one-way"
+                              :start-date "2020-08-22"
+                              :start-date-valid? true
+                              :end-date "2020-08-22"
+                              :end-date-valid? true})]
 
-    (fn []
-      [:section
-       [:h2 "Flight Booker"]
-       [:select {:on-change #(swap! state assoc :selected (-> % .-target .-value))
-                 :value (:selected @state)}
-        [:option {:value "one-way"
-                  :selected (= (:selected @state) "one-way")}
-         "one-way flight"]
+    [:section
+     [:h2 "Flight Booker"]
+     [:select {:on-change #(swap! state assoc :selected (-> % .-target .-value))
+               :value (:selected @state)}
+      [:option {:value "one-way"
+                :selected (= (:selected @state) "one-way")}
+       "one-way flight"]
 
-        [:option {:value "return"
-                  :selected (= (:selected @state) "return")}
-         "return flight"]]
+      [:option {:value "return"
+                :selected (= (:selected @state) "return")}
+       "return flight"]]
 
-       [:input {:type "date"
-                :value (:start-date @state)
-                :style {:background (when-not (:start-date-valid? @state)
-                                      "red")}
-                :on-change (fn [e]
-                             (let [value (-> e .-target .-value)
-                                   ;; try to parse the timestamp.
-                                   ;; If it's invalid, set ts to nil
-                                   ts (try (parse-timestamp value)
-                                           (catch :default _ nil))]
-                               (swap! state assoc :start-date-valid? (not (nil? ts)))
-                               (swap! state assoc :start-date value)))}]
+     [:input {:type "date"
+              :value (:start-date @state)
+              :style {:background (when-not (:start-date-valid? @state)
+                                    "red")}
+              :on-change (fn [e]
+                           (let [value (-> e .-target .-value)
+                                 ;; try to parse the timestamp.
+                                 ;; If it's invalid, set ts to nil
+                                 ts (try (parse-timestamp value)
+                                         (catch :default _ nil))]
+                             (swap! state assoc :start-date-valid? (not (nil? ts)))
+                             (swap! state assoc :start-date value)))}]
 
-       [:input {:type "date"
-                :value (:end-date @state)
-                :style {:background (when-not (:end-date-valid? @state)
-                                      "red")}
-                :on-change (fn [e]
-                             (let [value (-> e .-target .-value)
-                                   ;; try to parse the timestamp.
-                                   ;; If it's invalid, set ts to nil
-                                   ts (try (parse-timestamp value)
-                                           (catch :default _ nil))]
-                               (swap! state assoc :end-date-valid? (not (nil? ts)))
-                               (swap! state assoc :end-date value)))
-                ;; disable the end date for a one-way flight
-                :disabled (= (:selected @state) "one-way")}]
+     [:input {:type "date"
+              :value (:end-date @state)
+              :style {:background (when-not (:end-date-valid? @state)
+                                    "red")}
+              :on-change (fn [e]
+                           (let [value (-> e .-target .-value)
+                                 ;; try to parse the timestamp.
+                                 ;; If it's invalid, set ts to nil
+                                 ts (try (parse-timestamp value)
+                                         (catch :default _ nil))]
+                             (swap! state assoc :end-date-valid? (not (nil? ts)))
+                             (swap! state assoc :end-date value)))
+              ;; disable the end date for a one-way flight
+              :disabled (= (:selected @state) "one-way")}]
 
-       [:button {:type "button"
-                 :on-click #(swap! state
-                                   assoc :booking-message
-                                   (if (= (:selected @state) "one-way")
-                                     (str "You have booked a one-way flight on " (:start-date @state))
-                                     (str "You have booked a round-trip flight - out on " (:start-date @state) ", return on " (:end-date @state))))
-                 :disabled
-                 ;; if either date is not a valid date, disable booking
-                 (if-not (and (:end-date-valid? @state) (:start-date-valid? @state))
-                   true
-                   ;; otherwise, we know both dates are valid
-                   (let [start-date (parse-timestamp (:start-date @state))
-                         end-date (parse-timestamp (:end-date @state))]
-                     ;; if the end date is before the start date, disable
-                     (< end-date start-date)))}
+     [:button {:type "button"
+               :on-click #(swap! state
+                                 assoc :booking-message
+                                 (if (= (:selected @state) "one-way")
+                                   (str "You have booked a one-way flight on " (:start-date @state))
+                                   (str "You have booked a round-trip flight - out on " (:start-date @state) ", return on " (:end-date @state))))
+               :disabled
+               ;; if either date is not a valid date, disable booking
+               (if-not (and (:end-date-valid? @state) (:start-date-valid? @state))
+                 true
+                 ;; otherwise, we know both dates are valid
+                 (let [start-date (parse-timestamp (:start-date @state))
+                       end-date (parse-timestamp (:end-date @state))]
+                   ;; if the end date is before the start date, disable
+                   (< end-date start-date)))}
 
-        "Book"]
-       [:div (:booking-message @state)]])))
+      "Book"]
+     [:div (:booking-message @state)]]))
 
 (defn timer
   "The task is to build a frame containing a gauge G for the elapsed time e, a
@@ -205,57 +204,56 @@
   layout is to be done like suggested in the screenshot. In particular, L must
   occupy all the remaining space."
   []
-  (let [state (r/atom {:selected ""
-                       :people {}
-                       :filter ""
-                       :name ""
-                       :surname ""})]
-    (fn []
-      [:section
-       [:h2 "CRUD"]
-       ;; filter
-       [:fieldset {:style {:border "none"}}
-        [:label {:for "prefix"} "Filter prefix:"]
-        [:input {:type "text" :id "prefix" :value (:filter @state)
-                 :on-change #(swap! state assoc :filter (-> % .-target .-value))}]]
+  (r/with-let [state (r/atom {:selected ""
+                              :people {}
+                              :filter ""
+                              :name ""
+                              :surname ""})]
+    [:section
+     [:h2 "CRUD"]
+     ;; filter
+     [:fieldset {:style {:border "none"}}
+      [:label {:for "prefix"} "Filter prefix:"]
+      [:input {:type "text" :id "prefix" :value (:filter @state)
+               :on-change #(swap! state assoc :filter (-> % .-target .-value))}]]
 
-       ;; name select
-       [:select {:size 5
-                 :style {:width "50%"}
-                 :on-change #(swap! state assoc :selected (-> % .-target .-value))}
-        (let [prefix-filter (:filter @state)
-              people (filter (fn [[_ person]]
-                               (starts-with? (:surname person) prefix-filter))
-                            (:people @state))]
-         (for [[uuid person] people]
-           ^{:key uuid} [:option {:value uuid} (str (:surname person) ", " (:name person))]))]
+     ;; name select
+     [:select {:size 5
+               :style {:width "50%"}
+               :on-change #(swap! state assoc :selected (-> % .-target .-value))}
+      (let [prefix-filter (:filter @state)
+            people (filter (fn [[_ person]]
+                             (starts-with? (:surname person) prefix-filter))
+                          (:people @state))]
+       (for [[uuid person] people]
+         ^{:key uuid} [:option {:value uuid} (str (:surname person) ", " (:name person))]))]
 
-       ;; name input
-       [:fieldset {:style {:border "none"}}
-        [:label {:for "name"} "Name:"]
-        [:input {:type "text" :id "name" :value (:name @state)
-                 :on-change #(swap! state assoc :name (-> % .-target .-value))}]]
-       [:fieldset {:style {:border "none"}}
-        [:label {:for "surname"} "Surname:"]
-        [:input {:type "text" :id "surname" :value (:surname @state)
-                 :on-change #(swap! state assoc :surname (-> % .-target .-value))}]]
+     ;; name input
+     [:fieldset {:style {:border "none"}}
+      [:label {:for "name"} "Name:"]
+      [:input {:type "text" :id "name" :value (:name @state)
+               :on-change #(swap! state assoc :name (-> % .-target .-value))}]]
+     [:fieldset {:style {:border "none"}}
+      [:label {:for "surname"} "Surname:"]
+      [:input {:type "text" :id "surname" :value (:surname @state)
+               :on-change #(swap! state assoc :surname (-> % .-target .-value))}]]
 
-       ;; actions
-       [:button {:type "button"
-                 :on-click #(swap! state assoc-in [:people (str (random-uuid))] {:name (:name @state)
-                                                                                 :surname (:surname @state)})}
-        "Create"]
-       [:button {:type "button"
-                 :on-click #(swap! state assoc-in [:people (:selected @state)] {:name (:name @state)
-                                                                                :surname (:surname @state)})
-                 :disabled (= "" (:selected @state))}
-        "Update"]
-       [:button {:type "button"
-                 :on-click #(do
-                              (swap! state assoc :people (dissoc (:people @state) (:selected @state)))
-                              (swap! state assoc :selected ""))
-                 :disabled (= "" (:selected @state))}
-        "Delete"]])))
+     ;; actions
+     [:button {:type "button"
+               :on-click #(swap! state assoc-in [:people (str (random-uuid))] {:name (:name @state)
+                                                                               :surname (:surname @state)})}
+      "Create"]
+     [:button {:type "button"
+               :on-click #(swap! state assoc-in [:people (:selected @state)] {:name (:name @state)
+                                                                              :surname (:surname @state)})
+               :disabled (= "" (:selected @state))}
+      "Update"]
+     [:button {:type "button"
+               :on-click #(do
+                            (swap! state assoc :people (dissoc (:people @state) (:selected @state)))
+                            (swap! state assoc :selected ""))
+               :disabled (= "" (:selected @state))}
+      "Delete"]]))
 
 (defn circle-drawer
   "The task is to build a frame containing an undo and redo button as well as a
